1f5ffcf5c085e9b61628f264348f20401aa228b5
patil-vrushali <Vrushali.p@tecxlabs.com>
2023-06-18 00:19:24 +0530

    project added

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -7,18 +7,19 @@ const ProjectCardData=[
         source: "https://github.com/VrushaliDipakPatil/WeatherProject"
     },
     {
+        imgsrc: "https://images04.nicepage.com/feature/461183/website-blog.jpg",
+        title: "Blog Website",
+        text : "Welcome to Blog Creation Website where all users can read the blogs but only registered user can create the blog and delete or edit their own blogs.",
+        view: "https://blog-website-olive.vercel.app/",
+        source: "https://github.com/VrushaliDipakPatil"
+    },
+    {
         imgsrc: "https://images.unsplash.com/photo-1522204523234-8729aa6e3d5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8b25saW5lJTIwc2hvcHBpbmd8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
         title: "Online Shopping",
         text : "Online shopping is a form of electronic commerce which allows consumers to directly buy goods or services from a seller over the Internet using a web browser or a mobile app.",
         view: "https://e-commerce-react-h5kz7f4v1-vrushalidipakpatil.vercel.app/",
         source: "https://github.com/VrushaliDipakPatil/e-commerce"
     },
-    // {
-    //     imgsrc: "https://images.pexels.com/photos/240561/pexels-photo-240561.jpeg?auto=compress&cs=tinysrgb&w=600",
-    //     title: "project 3",
-    //     text : "uygdywgjwh iuwgdw iugygjbjdf ubhfc hbukcbj uybhc",
-    //     view: ""
-    // }
 ];
 
 export default ProjectCardData;
