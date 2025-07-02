0cdb772d367b149c49964278b4bb11f6b204e857
patil-vrushali <Vrushali.p@tecxlabs.com>
2025-07-02 19:10:39 +0530

    changeLog: updated

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,20 +1,20 @@
-import WomenFashion from "../assets/womenfashion.jpg"
+// import WomenFashion from "../assets/womenfashion.jpg"
 
 const ProjectCardData=[
-    {
-        imgsrc: "https://images04.nicepage.com/feature/461183/website-blog.jpg",
-        title: "Blog Website",
-        text : "Welcome to Blog Creation Website build on MERN (MongoDB, Express, React, Node) where all users can read the blogs but only registered user can create the blog and delete or edit their own blogs.",
-        view: "https://blog-website-olive.vercel.app/",
-        source: "https://github.com/VrushaliDipakPatil"
-    },
-    {
-        imgsrc: WomenFashion,
-        title: "Women Fashion",
-        text : "Welcome to the online shopping store for womens build on MERN (MongoDB, Express, React, Node) , styled components used for the CSS and stripe is used for payment.",
-        view: "https://mern-e-commerce-fs2hemcuo-vrushalidipakpatil.vercel.app/",
-        source: "https://github.com/VrushaliDipakPatil"
-    },
+    // {
+    //     imgsrc: "https://images04.nicepage.com/feature/461183/website-blog.jpg",
+    //     title: "Blog Website",
+    //     text : "Welcome to Blog Creation Website build on MERN (MongoDB, Express, React, Node) where all users can read the blogs but only registered user can create the blog and delete or edit their own blogs.",
+    //     view: "https://blog-website-olive.vercel.app/",
+    //     source: "https://github.com/VrushaliDipakPatil"
+    // },
+    // {
+    //     imgsrc: WomenFashion,
+    //     title: "Women Fashion",
+    //     text : "Welcome to the online shopping store for womens build on MERN (MongoDB, Express, React, Node) , styled components used for the CSS and stripe is used for payment.",
+    //     view: "https://mern-e-commerce-fs2hemcuo-vrushalidipakpatil.vercel.app/",
+    //     source: "https://github.com/VrushaliDipakPatil"
+    // },
     {
         imgsrc: "https://images.unsplash.com/photo-1522204523234-8729aa6e3d5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8b25saW5lJTIwc2hvcHBpbmd8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
         title: "Online Shopping",
