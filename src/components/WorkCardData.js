6b61537ffaa8e07cf2113fda29304508eceb6db4
patil-vrushali <Vrushali.p@tecxlabs.com>
2026-06-19 15:45:57 +0530

    updated portfolio

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -4,13 +4,6 @@ import MarketSeasonality from "../assets/marketseasonality.jpg"
 const ProjectCardData=[
     // {
     //     imgsrc: "https://images04.nicepage.com/feature/461183/website-blog.jpg",
-    //     title: "Blog Website",
-    //     text : "Welcome to Blog Creation Website build on MERN (MongoDB, Express, React, Node) where all users can read the blogs but only registered user can create the blog and delete or edit their own blogs.",
-    //     view: "https://blog-website-olive.vercel.app/",
-    //     source: "https://github.com/VrushaliDipakPatil"
-    // },
-    // {
-    //     imgsrc: WomenFashion,
     //     title: "Women Fashion",
     //     text : "Welcome to the online shopping store for womens build on MERN (MongoDB, Express, React, Node) , styled components used for the CSS and stripe is used for payment.",
     //     view: "https://mern-e-commerce-fs2hemcuo-vrushalidipakpatil.vercel.app/",
@@ -22,20 +15,6 @@ const ProjectCardData=[
         text : "A React-based financial dashboard displaying real-time and historical crypto data with interactive date range filters using MUI DatePicker and dayjs, and responsive design for both desktop and mobile",
         view: "https://market-seasonality-explorer-ttuy.vercel.app/",
         source: "https://github.com/VrushaliDipakPatil/market_seasonality_explorer"
-    },
-    {
-        imgsrc: "https://images.unsplash.com/photo-1522204523234-8729aa6e3d5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8b25saW5lJTIwc2hvcHBpbmd8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
-        title: "Online Shopping",
-        text : "Welcome to online shopping website which is a form of electronic commerce which allows consumers to directly buy goods or services from a seller over the Internet using a web browser or a mobile app.",
-        view: "https://e-commerce-react-h5kz7f4v1-vrushalidipakpatil.vercel.app/",
-        source: "https://github.com/VrushaliDipakPatil/e-commerce"
-    },
-    {
-        imgsrc: "https://www.shutterstock.com/image-illustration/graphical-digital-world-news-background-260nw-459780472.jpg",
-        title: "Weather Update",
-        text : "Welcome to weather forecast app, your go-to source for accurate and up-to-date weather information. With this app, you can access real-time weather data for your location and any location worldwide.",
-        view: "https://weather-project-ii7q8vhiw-vrushalidipakpatil.vercel.app/",
-        source: "https://github.com/VrushaliDipakPatil/WeatherProject"
     },
 ];
 
